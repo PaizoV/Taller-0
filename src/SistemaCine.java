51a22a11cc9b7d99111b603ab1c0bb7a42dcba0e
Maria-riv <maria.rivera01@alumnos.ucn.cl>
2021-09-23 20:44:05 -0300

    Lists

--- src/SistemaCine.java
+++ src/SistemaCine.java
@@ -2,6 +2,16 @@
 public class SistemaCine {
 
 	public static void main(String[] args) {
-		
+		//Listas
+		String [] nombres=new String[1000];
+		String [] apellidos=new String[1000];
+		String [] ruts= new String[1000];
+		String [] contrasenias=new String [1000];
+		String [] estados= new String [1000];
+		double [] saldos=new double[1000];
+		String [] nombresDePeliculas=new String[1000];
+		String [] tiposDePeliculas=new String[1000];
+		double [] recaudaciones= new double[1000];
+		String [] horarios= new String [1000];
 	}
 }
