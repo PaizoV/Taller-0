728be3d871b53e11beeedcf86dd7a94e33005f2a
Pablo <90540501+PaizoV@users.noreply.github.com>
2021-09-23 21:11:32 -0300

    Cambios menores al main

--- src/SistemaCine.java
+++ src/SistemaCine.java
@@ -1,21 +1,27 @@
+import java.util.Scanner;
 
 public class SistemaCine {
 
 	public static void main(String[] args) {
-		//Listas
-		String [] nombres=new String[1000];
-		String [] apellidos=new String[1000];
-		String [] ruts= new String[1000];
-		String [] contrasenias=new String [1000];
-		String [] estados= new String [1000];
-		double [] saldos=new double[1000];
-		String [] nombresDePeliculas=new String[1000];
-		String [] tiposDePeliculas=new String[1000];
-		double [] recaudaciones= new double[1000];
-		String [] horarios= new String [1000];
-		//Matrices
-		String [][] entradasCompradas= new String[1000][1000];
+		Scanner scan = new Scanner(System.in);
+		// Creacion de arreglos
 		
+		// Listas parelelas de clientes
+		String[] nombres = new String[1000];
+		String[] apellidos = new String[1000];
+		String[] ruts = new String[1000];
+		String[] contrasenias = new String [1000];
+		String[] estados = new String [1000];
+		double[] saldos = new double[1000];
 		
+		// Listas paralelas de peliculas
+		String[] nombresDePeliculas = new String[1000];
+		String[] tiposDePeliculas = new String[1000];
+		double[] recaudaciones = new double[1000];
+		String[] horarios = new String [1000];
+		
+		// Matrices de inventarios y asientos
+		String[][] entradasCompradas = new String[1000][1000];
+		String[][][] asientos = new String[7][30][6];
 	}
 }
